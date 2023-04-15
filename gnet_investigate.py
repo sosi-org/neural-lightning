a6cfc993c71080838b1c11e494536d569e31e2b4
Sohail Siadatnejad <sohale@gmail.com>
2023-04-15 20:42:56 +0100

    devops-scripting-auto-self paradigm

--- gnet_investigate.py
+++ gnet_investigate.py
@@ -1,5 +1,8 @@
 # a new paradigm!
 
+import subprocess as sp
+
+
 def ensure_or_create_vend(VNAME):
     # https://github.com/ninjaaron/replacing-bash-scripting-with-python
 
@@ -14,35 +17,36 @@ def ensure_or_create_vend(VNAME):
 
         exit()
       print('ok2')
+      return # the only safe return point
+
     except Exception as e:
 
-      import subprocess as sp
       proc = sp.run(['python3', '-m', 'venv', '--copies', VNAME])
       if proc.returncode != 0:
           print('failed')
           exit()
       print('run this again after:')
       print(f'source "./{VNAME}/bin/activate"')
-
       exit()
+
 def install_all_in_venv():
       #pip install torch --no-cache-dir
       #pip install torchvision  --no-cache-dir
       #pip install torchvision
+      proc = sp.run(['pip', 'install', 'torch'])
+      if proc.returncode != 0:
+          print('failed')
+          exit()
+      print('ok-install 1')  # interject (yield)
       proc = sp.run(['pip', 'install', 'torchvision'])
       if proc.returncode != 0:
           print('failed')
           exit()
-      proc = sp.run(['pip', 'install', 'torchvision'])
-      if proc.returncode != 0:
-          print('failed')
-          exit()
-      print('ok')
+      print('ok-install 2')  # interject (yield)
 
-      exit()
 # Can exit
 ensure_or_create_vend( "p3-for-me")
-exit()
+
 install_all_in_venv()
 
 exit()
