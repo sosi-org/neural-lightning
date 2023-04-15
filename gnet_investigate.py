496a09aeb781bb3bc9e5265a03900d650fe11d1b
Sohail Siadatnejad <sohale@gmail.com>
2023-04-15 21:29:44 +0100

    refactor: a fucntion to prcess image

--- gnet_investigate.py
+++ gnet_investigate.py
@@ -98,30 +98,34 @@ print('ready')
 
 print('Load and preprocess the image')
 
-from PIL import Image
-import torchvision.transforms as transforms
 
-image_path = './image-input/istockphoto-487522266-612x612.jpeg'
-image = Image.open(image_path)
-print(image)
+def process_image(image_path):
 
-transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-image = transform(image)
-image = image.unsqueeze(0)  # Add batch dimension
+  from PIL import Image
+  import torchvision.transforms as transforms
+
+  image = Image.open(image_path)
+  print(image)
+
+  transform = transforms.Compose([
+      transforms.Resize(256),
+      transforms.CenterCrop(224),
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+  ])
+  image = transform(image)
+  image = image.unsqueeze(0)  # Add batch dimension
 
 
 
-# Perform inference
-predictions = googlenet_model(image)
+  # Perform inference
+  predictions = googlenet_model(image)
 
-# Get the predicted class
-predicted_class = torch.argmax(predictions, dim=1).item()
+  # Get the predicted class
+  predicted_class = torch.argmax(predictions, dim=1).item()
 
-print("Predicted class:", predicted_class)
-print("Predicted class:", googlenet_labels[predicted_class])
-# Predicted class: bee
+  print("Predicted class:", predicted_class)
+  print("Predicted class:", googlenet_labels[predicted_class])
+  # Predicted class: bee
+
+process_image( './image-input/istockphoto-487522266-612x612.jpeg')
